a3ac6df5c8de70c87806faeeacc8e64a254b0ab7
ANJONKER <anja.jonker@capgemini.com>
2019-08-30 15:05:45 +0200

    Added log file

---
+++ src/main/java/logger/CommandLogger.java
@@ -0,0 +1,29 @@
+package logger;
+
+import interfaces.IOutputter;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+
+public class CommandLogger {
+    public void logCommand(String command, IOutputter outputter) {
+        try{
+            //Log the command in the log file
+            String path="D:\\Users\\commandLogs.txt";
+            File file = new File(path);
+
+            FileWriter fw = new FileWriter(file.getAbsoluteFile(), true);
+            BufferedWriter bw = new BufferedWriter(fw);
+
+            // Write in file
+            bw.append(command);
+            bw.newLine();
+
+            // Close connection
+            bw.close();
+        } catch (Exception e){
+            outputter.printLine("warning: this command could not be logged");
+        }
+
+    }
+}
