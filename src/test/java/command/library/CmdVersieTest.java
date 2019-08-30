bac0ddc7296cc0c25bc6a9b829d98f3cafbadbb0
Dijk <erik.van.dijk@capgemini.com>
2019-08-30 12:15:53 +0200

    added unit testing for versie function

---
+++ src/test/java/command/library/CmdVersieTest.java
@@ -0,0 +1,71 @@
+package command.library;
+
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+import helpers.TestHelper;
+import filesystem.File;
+
+public class CmdVersieTest extends CmdTest {
+    @Before
+    public void setUp() {
+        // Check this file structure in base class: crucial to understand the tests.
+        this.createTestFileStructure();
+
+        // Add all commands which are necessary to execute this unit test
+        // Important: Other commands are not available unless added here.
+        this.commandInvoker.addCommand(new CmdVersie("ver", this.drive));
+    }
+
+    @Test
+    public void CmdVersie_isRetruningOSVersion()
+    {
+        // given
+        String system = System.getProperty("os.name");
+
+        // when
+        executeCommand("ver");
+
+        // then
+        TestHelper.assertContains(system, testOutput.toString());
+    }
+
+    @Test
+    public void CmdVersie_slashw_isRetruningOSVersion()
+    {
+        // given
+        String system = System.getProperty("os.name");
+
+        // when
+        executeCommand("ver /w");
+
+        // then
+        TestHelper.assertContains("Team UNICORN!! :D", testOutput.toString());
+    }
+
+    @Test
+    public void CmdVersie_slashW_isRetruningOSVersion()
+    {
+        // given
+        String system = System.getProperty("os.name");
+
+        // when
+        executeCommand("ver /W");
+
+        // then
+        TestHelper.assertContains("Team UNICORN!! :D", testOutput.toString());
+    }
+
+    @Test
+    public void CmdVersie_withIncorrectParameter_isReturningError()
+    {
+        // given
+        String system = System.getProperty("os.name");
+
+        // when
+        executeCommand("ver /Wasdas");
+
+        // then
+        TestHelper.assertContains("unknown parameter, please use /w to get information about the team.", testOutput.toString());
+    }
+}
